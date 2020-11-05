f5fb2d3dbc93f3ac1af8592c9eadab50c31de4ee
Pablo Salce <salcepablo13@gmail.com>
2020-11-04 20:29:20 -0800

    Starting

---
+++ script.js
@@ -0,0 +1,12 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+//generates secret number to guess
+const generateTarget = () => Math.floor(Math.random() * 9);
+
+
+const compareGuesses = (userGuess, computerGuess, secretNumber) => {
+
+}
